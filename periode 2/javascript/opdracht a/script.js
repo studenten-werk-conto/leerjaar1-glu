94247c87c5e3e2907b7b4e5ae63d0c1e2a3c5c62
Constantijn van Hartesveldt <constantijn@vanhartesveldt.nl>
2020-12-04 13:33:15 +0100

    kgjg

---
+++ periode 2/javascript/opdracht a/script.js
@@ -0,0 +1,22 @@
+console.log ("js is ingeladen")
+
+function wakerworden(event) {
+    console.warn("je moet nu uit bed komen het is gotverdomme 6 uur")
+    console.log("je komt uit bed en gaat naar de keuken")
+    console.log("je pakt de boter en het brood en de hagelslag")
+    console.log("je legt alles terug")
+    console.log("------------------------------")
+    
+}
+function ov(event) {
+    console.log("je gaat met de fiets naar staion")
+    console.log("je gaat naar het spoor")
+    console.log("je stapt in de trein")
+    console.log("je stapt uit en gaat naar de bus")
+    console.log("je stapt ui de bus en gaat naar school")
+    console.log("-------------------------------")
+
+    
+}
+wakerworden();
+ov();
---
+++ periode 2/javascript/opdracht b/assets/js/script.js
@@ -0,0 +1,10 @@
+/*
+schrijf javascript voor:
+klik op button1 wijzigt de volgorde van nieuwsartikelen
+klik op button2 wijzigt de achtergrondkleur van nieuwsartikel: 3
+klik op button3 verwijdert het 1-na-laatste nieuwsartikel
+klik op button4 voegt een nieuw artikel toe op plek 1 met eigen invulling naar keuze, met minimaal 1 afbeelding 
+(de tekst mag lorum ipsum zijn, mag ook een eigen artikel schrijven)
+Maak een button5 aan, en kies zelf de functionaliteit voor deze button (het moet wel iets met de DOM doen)
+*/
+
