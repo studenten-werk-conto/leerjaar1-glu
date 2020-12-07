d5ea34715a3ccbca090981dfbe0922f18ec6c9c0
Constantijn van Hartesveldt <constantijn@vanhartesveldt.nl>
2020-12-07 08:47:23 +0100

    d

--- periode 2/javascript/opdracht b/assets/js/script.js
+++ periode 2/javascript/opdracht b/assets/js/script.js
@@ -7,4 +7,21 @@ klik op button4 voegt een nieuw artikel toe op plek 1 met eigen invulling naar k
 (de tekst mag lorum ipsum zijn, mag ook een eigen artikel schrijven)
 Maak een button5 aan, en kies zelf de functionaliteit voor deze button (het moet wel iets met de DOM doen)
 */
+var button1 = document.getElementById("button1")
+var button2 = document.getElementById("button2")
+var button3 = document.getElementById("button3")
+var button4 = document.getElementById("button4")
+var item3 = document.getElementById("item3")
+button1.addEventListener("click", button2);
+
+function button2() { //verandert kleur
+  console.log ("kleur veranderen");
+  item3.body.style.backgroundColor = "red"; 
+} 
+
+button2.addEventListener("click", button1); // verandert volgorde
+
+function button1() {
+  console.log ("random shit");
+} 
 
