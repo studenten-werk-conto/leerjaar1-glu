db291ba252bddb27afabdbc01502deaa106b8018
conto <constantijn@vanhartesveldt.nl>
2020-12-08 13:26:37 +0100

    werk dagie

---
+++ periode 2/javascript/opdracht 1/assets/script.js
@@ -0,0 +1,10 @@
+console.info("js is ingeladen")
+
+for (var index = 0; 10 < x; index++) {
+    for (var x = 0; 10 < x; x++) {
+        document.getElementById("container").innerHTML += "<div class='cell'></div>";
+        console.info("er wordt een ding geladen222")
+    }
+document.getElementById("container").innerHTML += "<br>";
+console.info("er wordt een ding geladen333")
+}
