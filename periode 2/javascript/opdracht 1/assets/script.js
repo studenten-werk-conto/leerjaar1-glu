001070346a89e8ae2bc150ebb8c9e31ad90ccdde
conto <constantijn@vanhartesveldt.nl>
2021-01-03 14:06:32 +0100

    coomit

--- periode 2/javascript/opdracht 1/assets/script.js
+++ periode 2/javascript/opdracht 1/assets/script.js
@@ -2,9 +2,14 @@ console.info("js is ingeladen")
 
 for (var index = 0; 10 < x; index++) {
     for (var x = 0; 10 < x; x++) {
-        document.getElementById("container").innerHTML += "<div class='cell'></div>";
-        console.info("er wordt een ding geladen222")
+        document.getElementById("container2").innerHTML += "<div class='cell2'></div>";
     }
-document.getElementById("container").innerHTML += "<br>";
-console.info("er wordt een ding geladen333")
+document.getElementById("container2").innerHTML += "<br>";
 }
+
+
+// hier onder vindt je alles wat overgetypt is 
+// row1 =["red","white","blue"];
+// for (var x = 0; 10 < row1.length; x++) {
+//     document.getElementById("container").innerHTML += "<div class='cell' style='background-color:"+ row1[1] +"></div>";
+// }
