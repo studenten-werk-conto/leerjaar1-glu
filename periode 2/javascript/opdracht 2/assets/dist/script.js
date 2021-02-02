9529cb1b6f962ceefc4afc1a2962cd606c125dda
conto <constantijn@vanhartesveldt.nl>
2021-02-02 09:13:13 +0100

    Auto stash before rebase of "refs/heads/master"

--- periode 2/javascript/opdracht 2/assets/dist/script.js
+++ periode 2/javascript/opdracht 2/assets/dist/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // const { Collisions } = require('detect-collisions');
 // https://www.youtube.com/watch?v=k238XpMMn38 ^^ importeerd een crash lib
 var trees; // maakt een var voor dat je hem oproept
@@ -5,10 +6,21 @@ var overlap = false; // als true wordt dan is de auto gechrashed
 var debug = true; // als de debug
 var snelheid = 100; // snelheid TODO FIX
 // let beweeg = document.getElementById("beweeg");
+=======
+// import * as dat from 'dat.gui';
+// const gui = new dat.GUI();
+// https://www.youtube.com/watch?v=k238XpMMn38 
+var trees; // maakt een var voor dat je hem oproept
+var overlap = false; // als true wordt dan is de auto gechrashed
+var debug = true; // als de debug 
+var snelheid = 100; // snelheid TODO FIX
+var beweeg = document.getElementById("beweeg");
+>>>>>>> Stashed changes
 var left = 100;
 var topp = 100;
 var car = document.getElementById("car"); // roept de auto in html naar js
 var crashcounter = document.getElementById("crashcounter");
+<<<<<<< Updated upstream
 var count = 0;
 console.log("js is ingeladen");
 for (var index = 0; index < 10; index++) {
@@ -24,6 +36,38 @@ for (var index = 0; index < 10; index++) {
 }
 car.style.left = left + "px";
 car.style.top = topp + "px";
+=======
+// var FizzyText = function () {
+//   this.message = 'dat.gui';
+//   this.snelheid = 0.8;
+//   this.displayOutline = false;
+//   this.explode = function () {
+//     console.log("je hebt iets aangeklikt in de cheat menu")
+//   };
+//   console.log("je hebt iets niet aan geklikt")
+//   //   // Define render logic ...TODO FIX THIS FUCKING MESS WOULDNT IMPORT
+// };
+// window.onload = function () {
+//   var text = new FizzyText();
+//   var gui = new dat.GUI();
+//   gui.add(text, 'message');
+//   gui.add(text, 'snelheid', -5, 5);
+//   gui.add(text, 'displayOutline');
+//   gui.add(text, 'explode');
+// };
+// // einde cheat menu
+for (var index = 0; index < 5; index++) { // spawnt bomen 
+    var img = document.createElement("img");
+    img.src = 'assets/img/tree.jpg';
+    img.className = 'tree';
+    // op line ↓ col 32 stond eerst 80 op line ↓↓ col 31 stond 80
+    img.style.left = Math.random() * 90 + '%';
+    img.style.top = Math.random() * 100 + '%';
+    document.getElementById("container").appendChild(img);
+}
+beweeg.style.left = left + "px";
+beweeg.style.top = topp + "px";
+>>>>>>> Stashed changes
 document.addEventListener("keydown", function logKey(e) {
     var key = e.key;
     if (debug == true) {
@@ -31,6 +75,7 @@ document.addEventListener("keydown", function logKey(e) {
     }
     if (key == "ArrowUp") {
         topp--;
+<<<<<<< Updated upstream
         car.style.transform = "rotate(-90deg)";
     }
     if (key == "ArrowDown") {
@@ -133,6 +178,36 @@ function crash() {
             }
         }
         return true;
+=======
+    }
+    if (key == "ArrowDown") {
+        topp++;
+    }
+    if (key == "ArrowLeft") {
+        left--;
+    }
+    if (key == "ArrowRight") {
+        left++;
+    }
+    //console.log(left + " : " + topp);  
+    beweeg.style.left = left + "px";
+    beweeg.style.top = topp + "px";
+});
+function chrash() {
+    var trees = document.getElementsByClassName("tree");
+    var overlap = false;
+    for (var index = 0; index < trees.length; index++) {
+        overlap = !(car.getBoundingClientRect().right < trees[index].getBoundingClientRect().left ||
+            car.getBoundingClientRect().left < trees[index].getBoundingClientRect().right ||
+            car.getBoundingClientRect().bottom < trees[index].getBoundingClientRect().top ||
+            car.getBoundingClientRect().top < trees[index].getBoundingClientRect().bottom);
+        if (overlap) {
+            crashcounter += "1";
+            car.src = './assets/img/car' + crashcounter + '.png';
+            console.log('crashcounter' + crashcounter);
+            return true;
+        }
+>>>>>>> Stashed changes
     }
     return overlap;
 }
