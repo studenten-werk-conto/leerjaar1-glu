38b85079995fc7e4b88cd548397303fd985ba32a
conto <constantijn@vanhartesveldt.nl>
2021-03-24 17:33:20 +0100

    commit

--- periode3/js/snake/server/main.js
+++ periode3/js/snake/server/main.js
@@ -3,8 +3,8 @@ const https = require('https');
 const WebSocket = require('ws');
 
 const server = https.createServer({
-  cert: fs.readFileSync('/path/to/cert.pem'),
-  key: fs.readFileSync('/path/to/key.pem')
+  cert: fs.readFileSync('./keys/cert.pem'),
+  key: fs.readFileSync('./keys/key.pem')
 });
 const wss = new WebSocket.Server({ server });
 
